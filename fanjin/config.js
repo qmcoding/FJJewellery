3fe4fe9d191aa55809aacffc5a9e6f85319199e2
abel <sunqiming@bupt.edu.cn>
2020-10-27 14:51:25 +0800

    first commit

---
+++ fanjin/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  version: "2.1.1",
+  note: '',
+  subDomain: "aicard", // 根据教程 https://www.yuque.com/apifm/doc/qr6l4m 查看你自己的 subDomain
+}
---
+++ fanjin/pages/news/main.js
@@ -0,0 +1,11 @@
+const WXAPI = require('apifm-wxapi')
+Page({
+  getSuggest: function (e) {
+    let keyword = e.detail.value;
+    console.log(keyword);
+    /*想要的功能自由发挥*/
+  },
+
+
+  
+})
